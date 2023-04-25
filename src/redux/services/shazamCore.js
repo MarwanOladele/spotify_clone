8d13fa029438f50b6f6134d6c18bcae2bd7fb085
MarwanOladele <oladelemarwan@gmail.com>
2023-04-25 13:50:16 +0100

    env implemented to hide key

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -7,7 +7,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", 'import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY');
+      headers.set("X-RapidAPI-Key", import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
       return headers;
     },
